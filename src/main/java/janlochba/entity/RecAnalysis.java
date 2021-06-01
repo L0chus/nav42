cb22a2a80deb9cc7de890f1f1687939a7ae2ab70
L0chus <Janloch93@gmail.com>
2021-06-01 09:46:23 +0200

    RecImprove und RecAnalysis sind drin Recommendation.java ist raus

--- src/main/java/janlochba/entity/RecAnalysis.java
+++ src/main/java/janlochba/entity/RecAnalysis.java
@@ -7,11 +7,13 @@ import javax.persistence.Id;
 import java.util.Objects;
 
 @Entity
-public class Recommendation {
+public class RecAnalysis {
     private String id;
     private String name;
     private String description;
     private String typ;
+    private String extra1;
+    private String extra2;
 
     @Id
     @Column(name = "id")
@@ -53,16 +55,36 @@ public class Recommendation {
         this.typ = typ;
     }
 
+    @Basic
+    @Column(name = "extra1")
+    public String getExtra1() {
+        return extra1;
+    }
+
+    public void setExtra1(String extra1) {
+        this.extra1 = extra1;
+    }
+
+    @Basic
+    @Column(name = "extra2")
+    public String getExtra2() {
+        return extra2;
+    }
+
+    public void setExtra2(String extra2) {
+        this.extra2 = extra2;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Recommendation that = (Recommendation) o;
-        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(description, that.description) && Objects.equals(typ, that.typ);
+        RecAnalysis that = (RecAnalysis) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(description, that.description) && Objects.equals(typ, that.typ) && Objects.equals(extra1, that.extra1) && Objects.equals(extra2, that.extra2);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, description, typ);
+        return Objects.hash(id, name, description, typ, extra1, extra2);
     }
 }
---
+++ src/main/java/janlochba/entity/RecImprove.java
@@ -0,0 +1,92 @@
+package janlochba.entity;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class RecImprove {
+    private Integer id;
+    private String name;
+    private String description;
+    private String typ;
+    private String extra1;
+    private String extra2;
+
+    @Id
+    @Basic
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "Name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "Description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Basic
+    @Column(name = "Typ")
+    public String getTyp() {
+        return typ;
+    }
+
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+
+    @Basic
+    @Column(name = "extra1")
+    public String getExtra1() {
+        return extra1;
+    }
+
+    public void setExtra1(String extra1) {
+        this.extra1 = extra1;
+    }
+
+    @Basic
+    @Column(name = "extra2")
+    public String getExtra2() {
+        return extra2;
+    }
+
+    public void setExtra2(String extra2) {
+        this.extra2 = extra2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RecImprove that = (RecImprove) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(description, that.description) && Objects.equals(typ, that.typ) && Objects.equals(extra1, that.extra1) && Objects.equals(extra2, that.extra2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, typ, extra1, extra2);
+    }
+
+}
